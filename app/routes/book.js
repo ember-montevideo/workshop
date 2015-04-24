fd671559ea80fea1a1e31535e78a4f18d6e8d5a5
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2015-04-24 17:07:49 -0300

    Navigate to book details and back

--- app/routes/book.js
+++ app/routes/book.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var index = Number(params.id) - 1;
+    var index = Number(params.id);
 
     return this.modelFor('books')[index];
   }
