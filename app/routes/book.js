ad3648e55c904f48c1e1dce4f8ec88cfffd0a07f
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2015-04-24 19:06:04 -0300

    Convert models to ember data

--- app/routes/book.js
+++ app/routes/book.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    var index = Number(params.id);
-
-    return this.modelFor('books')[index];
+    return this.store.find('book', params.id);
   }
 });
--- app/routes/books.js
+++ app/routes/books.js
@@ -1,19 +1,13 @@
 import Ember from 'ember';
 
-var books = [
-  { title: 'El proceso', author: { firstName: 'Franz', lastName: 'Kafka' }},
-  { title: 'Rebelión en la granja', author: { firstName: 'George', lastName: 'Orwell' }},
-  { title: '1984', author: { firstName: 'George', lastName: 'Orwell' }}
-];
-
 export default Ember.Route.extend({
   model: function() {
-    return books;
+    return this.store.find('book');
   },
 
   actions: {
     createBook: function(book) {
-      books.addObject(book);
+      // books.addObject(book);
     }
   }
 });
